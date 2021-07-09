7f3f1878d69c8787236baff77b0e1967818fda68
M1k3-404 <86404061+M1k3-404@users.noreply.github.com>
2021-07-09 18:35:21 +0530

    Update PlayerSelection.java

--- tute07(prog)/src/tute07/tute07/PlayerSelection.java
+++ tute07(prog)/src/tute07/tute07/PlayerSelection.java
@@ -1,17 +1,21 @@
 package tute07;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class PlayerSelection {
     public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<Player>();
+
         Scanner sc = new Scanner(System.in);
 
         boolean continueLoop = true;
         while(continueLoop == true) {
             System.out.println("Enter 'A' to add a player.");
             System.out.println("Enter 'V' to view all players.");
-            System.out.println("Enter 'B' to view best two batmans.");
-            System.out.println("Enter 'C' to view best two bowlers.");
+            System.out.println("Enter 'B' to view best batman.");
+            System.out.println("Enter 'C' to view best bowler.");
             System.out.println("Enter 'E' to exit the program.");
             System.out.print("Enter the code of the function you want to access: ");
             String accessedFunction = sc.nextLine();
@@ -26,34 +30,51 @@ public class PlayerSelection {
                 String type = sc.nextLine();
                 System.out.print("Enter Player batting-style: ");
                 String battingStyle = sc.nextLine();
-
                 System.out.print("Enter runs scored by the player in last 15 matches: ");
-                int playerRuns = sc.nextInt();
+                int score = sc.nextInt();
                 System.out.print("Enter the number of wickets taken by the player in last 15 matches: ");
-                int playerWickets = sc.nextInt();
+                int wickets = sc.nextInt();
+                System.out.println("");
 
-                int[] statistics = new int[2];
-                statistics[0] = playerRuns;
-                statistics[1] = playerWickets;
+                Player newPlayer = new Player(name, age, type, score, wickets, battingStyle);
 
-                Player newPlayer = new Player(name, age, type, statistics, battingStyle);
-
-                System.out.println("Player Name: " + name);
-                System.out.println("Player age: " + age);
-                System.out.println("Player type: " + type);
-                System.out.println("Runs scored in last 15 matches: " + statistics[0]);
-                System.out.println("Wickets taken in last 15 matches: " + statistics[1]);
-                System.out.println("Batting style: " + battingStyle);
+                newPlayer.viewDetails();
+                players.add(newPlayer);
 
             } else if (accessedFunction.equalsIgnoreCase("V")) {
-                //
+                for(int i = 0; i < players.size(); i++) {
+                    players.get(i).viewDetails();
+                }
+
             } else if (accessedFunction.equalsIgnoreCase("B")) {
-                //
+                int maxRuns = 0;
+                for(int i = 0; i < players.size(); i++) {
+                    for(int j = 0; j < players.size(); j++) {
+                        if(players.get(maxRuns).score < players.get(j).score) {
+                            maxRuns = j;
+                        }
+                    }
+                }
+
+                System.out.println("Best batman: " + players.get(maxRuns).name);
+
             } else if (accessedFunction.equalsIgnoreCase("C")) {
-                //method
+                int maxWickets = 0;
+                for(int i = 0; i < players.size(); i++) {
+                    for(int j = 0; j < players.size(); j++) {
+                        if(players.get(maxWickets).wickets < players.get(j).wickets) {
+                            maxWickets = j;
+                        }
+                    }
+                }
+
+                System.out.println("Best bowler: " + players.get(maxWickets).name);
+
             } else if (accessedFunction.equalsIgnoreCase("E")) {
-                //method
+                System.out.println("Thank you for using our system.");
+                System.out.println("Have a nice day!");
                 continueLoop = false;
+
             } else {
                 System.out.println("Code you entered doesn't belong to any function.");
             }
