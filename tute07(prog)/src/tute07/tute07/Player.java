cfe34334a83a2ab63681bc5835896684d2e43813
M1k3-404 <86404061+M1k3-404@users.noreply.github.com>
2021-07-09 18:35:28 +0530

    Update Player.java

--- tute07(prog)/src/tute07/tute07/Player.java
+++ tute07(prog)/src/tute07/tute07/Player.java
@@ -4,47 +4,29 @@ import java.util.*;
 
 public class Player {
 
-    private String name;
+    public String name;
     private int age;
     private String type;
-    private int[] statistics;
+    public int score;
+    public int wickets;
     private String battingStyle;
 
-    public Player(String name, int age,String type, int[] statistics, String battingStyle) {
+    public Player(String name, int age,String type, int score, int wickets, String battingStyle) {
         this.name = name;
         this.age = age;
         this.type = type;
-        this.statistics = statistics;
+        this.score = score;
+        this.wickets = wickets;
         this.battingStyle = battingStyle;
     }
 
-    public double average() {
-        double sum = Arrays.stream(statistics).average().getAsDouble();
-        return sum;
-    }
-
-    public int bestPerformance(){
-        int max = 0;
-        for (int i = 0; i < statistics.length; i++){
-            if (max < statistics[i]){
-                max = statistics[i];
-            }
-        }
-        return max;
-    }
-
-    public String playerType(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter player type:");
-        String playerType = input.next();
-        return playerType;
-    }
-
-    public String battingStyle(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter batting style:");
-        String battingStyle = input.next();
-        return battingStyle;
+    void viewDetails() {
+        System.out.println("Player Name: " + name);
+        System.out.println("Player age: " + age);
+        System.out.println("Player type: " + type);
+        System.out.println("Runs scored in last 15 matches: " + score);
+        System.out.println("Wickets taken in last 15 matches: " + wickets);
+        System.out.println("Batting style: " + battingStyle);
     }
 
 
